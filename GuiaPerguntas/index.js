59e7bd47e8c8925f964e68164ca9bd583a4aff12
Andrielysson S. Barbosa <andrielysson.barbosa@gmail.com>
2023-03-12 22:15:37 -0300

    Add bootstrap

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -2,31 +2,15 @@ const express = require('express');
 const app = express();
 
 // USANDO EJS COMO VIEW ENGINE NO LUGAR DE HTML
+app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get(
     '/',
     function (req, res) {
-        var nome = 'Andri';
-        var lang = 'JS';
-        var exibirMsg = false;
-        var produtos = [
-            {nome: 'Doritos', preco: 3.14},
-            {nome: 'Coca-cola', preco: 5},
-            {nome: 'Leite', preco: 1.45}
-        ];
-
-        res.render(
-            'index.ejs', 
-            {
-                nome, 
-                lang,
-                exibirMsg,
-                produtos
-            }
-        );
+        res.render('index.ejs');
     }
 );
 
-app.listen(1212);
+app.listen(1111);
